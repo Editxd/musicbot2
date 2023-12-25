ef4d3d65f75685f16f2bdbfea0e10b69caead2b1
Editxd <152770568+Editxd@users.noreply.github.com>
2023-12-25 13:10:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ API_ID = API_ID = 29403305
 
 API_HASH = os.environ.get("API_HASH", "54c435573d0f2d9dada270a0cb493b36")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6452618086:AAFrRQM-VSXqsiyJ9IwnH6LgXh_kjRqrS6s")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6460187967:AAE2tRPt8kmVtTrMHppZP8YcjVZLb4UmPpo")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6381511858))
+OWNER = int(os.environ.get("OWNER", 1476002847))
 
-LOG = -1002080881254
+LOG = -1002145325377
 
